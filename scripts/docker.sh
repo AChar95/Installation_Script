ac11f984c39ceaaf54a520dd90ab6bd9a58c73d5
AChar95 <allancharles95@outlook.com>
2019-08-05 23:14:54 +0000

    sorted permissions on working installation scripts

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 curl https://get.docker.com | sudo bash
 sudo groupadd docker
-sudo usermod -aG jenkins docker
-sudo gpasswd -a jenkins docker
+sudo usermod -aG ${USER} docker
+sudo gpasswd -a ${USER} docker
 newgrp docker
 echo "************************** Please restart your system **************************"
 
